f09311afb318f6bf777fb0dd28f7e746db77416e
Nick's ACER Laptop <nicksaric@gmail.com>
2016-09-04 22:00:52 -0700

    Final-copy.

--- Homework1/app/src/main/java/cs175homeworks/homework1/MainActivity.java
+++ Homework1/app/src/main/java/cs175homeworks/homework1/MainActivity.java
@@ -2,12 +2,45 @@ package cs175homeworks.homework1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+  String[] fortuneList;
+  TextView resultArea;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    fortuneList = setFortunes();
+    resultArea = (TextView) findViewById(R.id.resultArea);
+    Button button = (Button) findViewById(R.id.fortuneButton);
+    button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        resultArea.setText(RandomFortune());
+      }
+    });
+  }
+
+  private String[] setFortunes() {
+    String[] newList = new String[8];
+    newList[0] = getResources().getString(R.string.fortuneOne);
+    newList[1] = getResources().getString(R.string.fortuneTwo);
+    newList[2] = getResources().getString(R.string.fortuneThree);
+    newList[3] = getResources().getString(R.string.fortuneFour);
+    newList[4] = getResources().getString(R.string.fortuneFive);
+    newList[5] = getResources().getString(R.string.fortuneSix);
+    newList[6] = getResources().getString(R.string.fortuneSeven);
+    newList[7] = getResources().getString(R.string.fortuneEight);
+    return newList;
+  }
+
+  private String RandomFortune() {
+    return fortuneList[(int) (Math.random() * 8)];
   }
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# CS175HW1
-Homework1
+1 Name: Nikola Saric
+2 Email: nicksaric@gmail.com
+3 Team: Individual assignment.
+4 Time: About an hour.
+5 Recommended version and screen size: API 19 or higher and any-size screen can be used.
+6 Hi! Hope you enjoyed it.
